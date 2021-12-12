bab9df5e200db0fa896ab86bb59e7f39739da63d
Rachit Sharma <rachitsharma3642@gmail.com>
2021-12-12 23:41:17 +0530

    Added Sign Up Page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path=require("path")
 const app=express()
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 app.set('views',path.join(__dirname,'/views'))
 
 app.listen(3000,()=>{
@@ -20,7 +21,7 @@ app.get('/register',(req,res)=>{
     res.render('signup.ejs')
 })
 
-app.get('/dashboard',(req,res)=>{
+app.post('/dashboard',(req,res)=>{
     res.render('dashboard.ejs')
 })
 
